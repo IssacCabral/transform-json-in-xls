eab172164012952202e6abb2fc1d432acf2b2d1e
Issac Cabral <issaccabrall@gmail.com>
2023-08-22 14:28:57 -0300

    refactor: formatando dinamicamente de acordo com os campos

--- src/service/xlsService.js
+++ src/service/xlsService.js
@@ -4,41 +4,53 @@ const path = require("path");
 
 class XlsxService {
   async createWorkSheet() {
-    const jsonData = await getProposals();
+    //const jsonData = await getProposals();
+    const jsonData = [
+      {
+        name: "Matias",
+        createdAt: 123456789,
+        age: 17,
+        updatedAt: 567890123,
+      },
+      {
+        name: "Maria",
+        createdAt: 321654987,
+        age: 22,
+        updatedAt: 567890123,
+      },
+    ];
     const workSheet = XLSX.utils.json_to_sheet(jsonData);
     return workSheet;
   }
 
   formatSheet(workSheet) {
-    workSheet["F2"].z = "0";
-    workSheet["F3"].z = "0";
-    workSheet["F4"].z = "0";
+    const columnsToFormat = ["createdAt", "updatedAt"];
+    const columnsToFormatPositions = [];
+
+    var ref = XLSX.utils.decode_range(workSheet["!ref"]);
+
+    // obter as posições das colunas que deverão ser formatadas
+    for (var columnPosition = 0; columnPosition <= ref.e.c; ++columnPosition) {
+      const columnName =
+        workSheet[XLSX.utils.encode_cell({ r: 0, c: columnPosition })].v;
+
+      if (columnsToFormat.includes(columnName)) {
+        columnsToFormatPositions.push(columnPosition);
+      }
+    }
+
+    // Iterando em cada linha das posições das colunas
+    columnsToFormatPositions.forEach((column) => {
+      for (var rowPosition = 1; rowPosition <= ref.e.r; ++rowPosition) {
+        workSheet[XLSX.utils.encode_cell({ r: rowPosition, c: column })].z =
+          "0";
+      }
+    });
   }
 
   createWorkBook(workSheet, sheetName) {
     const workBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workBook, workSheet, sheetName);
-    var ref = XLSX.utils.decode_range(workBook.Sheets[sheetName]["!ref"]);
-
-    const columns = [];
-
-    /**
-     * Vou precisar iterar pelas colunas, e buscar as posições que tiverem
-     * os campos createdAt, updatedAt
-     *
-     * Após isso, eu faço uma interação nessas colunas, para alterar célula por célula
-     * realizando a formatação correta
-     */
-    // obter os nomes das colunas
-    for (var columnPosition = 0; columnPosition <= ref.e.c; ++columnPosition) {
-      columns.push(
-        workBook.Sheets[sheetName][
-          XLSX.utils.encode_cell({ r: 0, c: columnPosition })
-        ].v
-      );
-    }
-
-    console.log({ columns });
 
     return workBook;
   }
